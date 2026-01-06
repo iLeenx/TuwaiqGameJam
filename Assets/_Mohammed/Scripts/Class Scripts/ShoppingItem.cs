b9dd242a1caeed9332c5c05df61f333a562efaef
Mohammed Soqati <hmadahmody360@gmail.com>
2026-01-06 20:01:44 +0300

    Added Coin Collection and Game Manager

---
+++ Assets/_Mohammed/Scripts/Class Scripts/ShoppingItem.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class ShoppingItem
+{
+    public Sprite icon;
+    public string itemName;
+    public int price;
+    public bool isStolen = false;
+}
