64b96bd5b78d299a34bae94a5f538ba1213c3221
iLeenx <LeenALshehrii@hotmail.com>
2026-01-07 11:51:41 +0300

    ending system - works
    4 endings and a reset

---
+++ Assets/_Leen/Ending/EndingManager.cs
@@ -0,0 +1,168 @@
+using UnityEngine;
+using TMPro;
+
+public class EndingManager : MonoBehaviour
+{
+    public static EndingManager Instance;
+
+    public int boughtCount = 0;
+    public int stolenCount = 0;
+
+    bool endingDecided = false;
+
+    public bool playerCaughtByGuard = false;
+
+
+    // view results in ui
+    //public TMP_Text boughtText;
+    //public TMP_Text stolenText;
+    //public TMP_Text endingResults;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void RegisterItem(ItemObtainType type)
+    {
+        if (type == ItemObtainType.Bought)
+        {
+            boughtCount++;
+            Debug.Log("Bought count: " + boughtCount);
+        }
+            
+        else
+        {
+            stolenCount++;
+            Debug.Log("Stolen count: " + stolenCount);
+        }
+    }
+
+    public void DecideEnding()
+    {
+        // to call this 
+        // EndingManager.Instance.DecideEnding();
+    
+
+        // to avoid multiple calls
+        if (endingDecided == true ) return;
+
+        endingDecided = true;
+
+        if (playerCaughtByGuard)
+        {
+            LoadCaughtEnding();
+            return;
+        }
+
+        if (stolenCount > boughtCount)
+        {
+            LoadStealEnding();
+        }
+        else if (boughtCount > stolenCount)
+        {
+            LoadGoodEnding();
+        }
+        else
+        {
+            DecideRandomEnding();
+        }
+    }
+
+
+    void DecideRandomEnding()
+    {
+        float chance = Random.value; // 0 to 1
+        Debug.Log("Random chance: " + chance);
+
+        if (chance < 0.5f)
+        {
+            LoadGoodEnding();
+        }
+        else
+        {
+            LoadStealEnding();
+        }
+    }
+
+    public void ResetEndingData()
+    {
+        // in main menu or restart
+        boughtCount = 0;
+        stolenCount = 0;
+        playerCaughtByGuard = false;
+        endingDecided = false;
+        
+        Debug.Log("Ending data reset.");
+    }
+
+    public void OnPlayerCaught()
+    {
+        playerCaughtByGuard = true;
+    }
+
+    public void TriggerEscapeEnding()
+    {
+        if (endingDecided == true) return;
+
+        endingDecided = true;
+
+        if (playerCaughtByGuard)
+        {
+            LoadCaughtEnding();
+        }
+        else
+        {
+            LoadEscapeEnding();
+        }
+    }
+
+
+    void LoadStealEnding() 
+    { 
+        Debug.Log("STEAL ENDING"); 
+    }
+    void LoadGoodEnding() 
+    { 
+        Debug.Log("GOOD ENDING");
+    }
+    void LoadCaughtEnding() 
+    { 
+        Debug.Log("CAUGHT ENDING"); 
+    }
+    void LoadEscapeEnding() 
+    { 
+        Debug.Log("ESCAPE ENDING");
+    }
+
+    // ending signals
+
+    // if bought button is clicked
+    // EndingManager.Instance.RegisterItem(ItemObtainType.Bought);
+    // if stolen button is clicked
+    // EndingManager.Instance.RegisterItem(ItemObtainType.Stolen);
+
+
+    // house door trigger / interaction -> here it start counting endings
+    // EndingManager.Instance.DecideEnding();
+
+
+    // when the NavMesh guard catches the player using trigger or collision
+    // EndingManager.Instance.OnPlayerCaught();
+    // if we want an immediate ending on caught, we can call this also
+    // EndingManager.Instance.DecideEnding();
+
+
+    // if player leaves the village area without being caught - used in a trigger
+    // EndingManager.Instance.TriggerEscapeEnding();
+
+
+}
